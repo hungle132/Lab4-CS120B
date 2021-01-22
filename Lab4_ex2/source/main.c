04e73181fdad0962223e35c8ab71efb52a4b1d51
Ubuntu <hungiscoolguy@yahoo.com>
2021-01-22 04:14:42 -0800

    cont

--- Lab4_ex2/source/main.c
+++ Lab4_ex2/source/main.c
@@ -12,12 +12,71 @@
 #include "simAVRHeader.h"
 #endif
 
+
+enum INC_States {Start,wait,plus,minus,both} INC_State;
+
+unsigned char val;
+unsigned char tmpA1;
+unsigned char tmpA2;
+
+void State_Machine(){//transitions
+	switch(INC_State){
+	tmpA1 = PINA & 0x01;
+	tmpA2 = PINA & 0x02;
+	
+		case Start:
+		INC_State = wait;
+		break;
+
+		case wait:
+		break;
+
+		case plus:
+		break;
+
+		case minus:
+		break;
+
+		case both:
+		break;
+
+		default:
+		INC_State = Start;
+		break;
+	}
+	switch(INC_State){
+		case Start:
+			PORTC = 7;
+		break;
+		case wait:
+		break;
+		case plus:
+		if(PORTC < 9){
+		PORTC = PORTC + 1;
+		}
+		break;
+		case minus:
+		if (PORTC > 0){
+		PORTC = PORTC - 1;
+		}
+		break;
+		case both:
+		PORTC = 0;
+		break;
+		default:
+		break;
+	}
+}
+
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+DDRA = 0x00; PORTA = 0xFF; // Configure port A's  pins as inputs
+	DDRC = 0xFF; PORTC = 0x00;
     /* Insert your solution below */
+	PORTC = 7;
     while (1) {
-
+	State_Machine();
     }
     return 1;
 }
